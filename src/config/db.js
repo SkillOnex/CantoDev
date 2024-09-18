4e5a34e0f63fdbd63a45e59ecffcc08874517bbb
SkillOnex <71238548+SkillOnex@users.noreply.github.com>
2024-09-18 15:29:16 -0300

    #6 Update - Novo print , fluxo do db

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql', // Altere de 'postgres' para 'mysql'
+  
   logging: false,
 });
 
